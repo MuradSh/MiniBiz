46346d6dfb1e9e32f3a76e614e913f7ab72e7721
Murad <muradshahmamadli@gmail.com>
2021-11-19 22:08:27 -0800

    Login

---
+++ lib/startup/login.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:minibiz/widgets/TextFieldEnhanced.dart';
+import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
+
+class login extends StatefulWidget {
+  const login({Key? key}) : super(key: key);
+
+  @override
+  _loginState createState() => _loginState();
+}
+
+class _loginState extends State<login> {
+
+  double _mediaHeight = 0;
+  double _mediaWidth = 0;
+
+  // FirebaseAuth _auth = FirebaseAuth.instance;
+  final _phoneFieldController = TextEditingController();
+  final _passFieldController = TextEditingController();
+  
+  @override
+  Widget build(BuildContext context) {
+    
+    _mediaHeight = MediaQuery.of(context).size.height;
+    _mediaWidth = MediaQuery.of(context).size.width;
+    
+    return Scaffold(
+        body: Container(
+          child:SafeArea(
+            child: Column(
+              children: [   Image(
+                image: AssetImage("assets/Minibiz.png"),
+                height: _mediaHeight*0.25,
+                width: _mediaHeight*0.25,
+              ),
+
+                Text(
+                  "Welcome Back",
+                  style: TextStyle(
+                      fontWeight: FontWeight.normal,
+                      fontSize: 40,
+                      fontFamily: 'Quicksand'
+                  ),
+                ),
+                SizedBox(height:  _mediaHeight*0.004),
+                Text(
+                  "Login To Continue",
+                  style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 20,
+                      fontFamily: 'Quicksand'
+                  ),
+                ),
+                ClipPath(
+                  clipper: WaveClipperTwo(reverse: true),
+                  child: Container(
+                    height: 220,
+                    color: Colors.red,
+                    child: Center(child: Text("OctagonalClipper()")),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        )
+    );
+  }
+}
---
+++ lib/widgets/TextFieldEnhanced.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+typedef void MyCallback(String addy);
+class TextFieldEnhanced extends StatelessWidget {
+
+  String placeholder="";
+  IconData icon;
+  bool passField;
+  double fontSize;
+  EdgeInsets paddingText;
+  FocusNode? focusNode;
+  TextEditingController? controller = null;
+  MyCallback? onChange;
+
+  TextFieldEnhanced(this.placeholder,this.icon, {
+    this.passField = false,
+    this.fontSize = -1,
+    this.paddingText = const EdgeInsets.fromLTRB(10, 10, 20, 10),
+    this.focusNode,
+    this.controller,
+    this.onChange
+  });
+
+  double _mediaHeight = 0;
+  double _mediaWidth = 0;
+
+  @override
+  Widget build(BuildContext context) {
+
+    _mediaHeight = MediaQuery.of(context).size.height;
+    _mediaWidth = MediaQuery.of(context).size.width;
+
+    return TextField(
+      decoration: InputDecoration(
+        hintText: placeholder,
+        hintStyle: TextStyle(fontSize: (fontSize==-1 ? _mediaHeight*0.03 : fontSize), color: Color(0xffa1a1a1)),
+        contentPadding: this.paddingText,
+        prefixIcon: Icon(this.icon,size: (fontSize==-1 ? _mediaHeight*0.04 : fontSize*1.01),color: Color(0xffa1a1a1)),
+        enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(
+                width: 1.5,
+                color: Color(0xffb2b2b2)
+            )
+        ),
+        focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(
+                width: 2.5,
+                color: Color(0xff818181)
+            )
+        ),
+      ),
+      style: TextStyle(
+          fontSize: (fontSize==-1 ? _mediaHeight*0.03 : fontSize),
+          color: Color(0xff111111),
+          fontFamily: 'Quicksand'
+      ),
+      enableSuggestions: false,
+      autocorrect: false,
+      obscureText: passField,
+      focusNode: focusNode,
+      controller: controller,
+      onChanged: onChange,
+    );
+  }
+}
